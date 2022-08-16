7e3ce1759b86ba43c280bf32359b6c0da0bbd674
xdev1 <xdev1.github@gmail.com>
2022-08-16 22:07:31 +0200

    Updated storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,29 @@
+const { mergeConfig } = require("vite");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/html",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/html",
+  core: {
+    builder: "@storybook/builder-vite",
+    disableTelemetry: true,
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      resolve: {
+        alias: {
+          "novo-ui": "src/main/core",
+        },
+      },
+    });
+  },
+};
